1d21d34adda46b3374520a15c5b419252e133773
Artem Vlakh <vlakhartem@gmail.com>
2022-06-04 09:25:13 +0200

    tab updated

--- 27/assets/js/main.js
+++ 27/assets/js/main.js
@@ -1,13 +1,11 @@
 $(function() {
     $('.card_header div').on('click', function(){
         if (!$(this).hasClass('active')) {
-            $('.card_header div.active').removeClass('active');
+            $('.card_header div.active, .tab_block div.active').removeClass('active');
             $(this).addClass('active');
-            $('.tab_block div.active').removeClass('active');
             $(".tab_block div[data-id='"+$(this).attr("id")+"']").addClass('active')
         }
     })
-    // ЩЕ РАЗ ДЯКУЮ ВСЕ ПРАЦЮЄ ОМГ
     $(".game-rating").starRating({
     starSize: 25
     });
