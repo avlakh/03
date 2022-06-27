ca6716ca387a1606c80fbcdb34f976af486c05dc
Artem Vlakh <vlakhartem@gmail.com>
2022-06-27 23:22:40 +0200

    project done

--- 36/assets/js/movies.js
+++ 36/assets/js/movies.js
@@ -5,7 +5,6 @@ const movieItem = {
             this.$emit('getMovie', id)
         },
         addRemoveFavorites (id){
-            // спосіб з перентом можливий, але так роблять джуни, бо хтось може змінити метод вище і все може зіпсуватись
             this.$emit('addRemove', id)
         }
     },
@@ -16,27 +15,37 @@ const App = {
     data() {
         return {
             API_KEY: '294b52ec',
-            search: 'Batman',
-            selected: '///',
+            search: '',
             movieList: [],
             movieInfo: {},
+            movieType: 'movie',
             showModal: false,
             favorites: [],
             showFavoritesList: false,
             searchResult: false
         }
+    }, components: {
+        movieItem
     }, methods: {
         searchMovies(){
             if (this.search !== '') {
-                axios.get(`https://www.omdbapi.com/?apikey=${this.API_KEY}&s=${this.search}`)
+                this.selectCategory()
+                axios.get(`https://www.omdbapi.com/?apikey=${this.API_KEY}&s=${this.search}&type=${this.movieType}`)
                     .then(response => {
                         this.movieList = response.data.Search
                         this.searchResult = true;
-                        this.search = '';
+                        this.showFavoritesList = false;
                     })
                     .catch(err=>{
                         console.log(err);
                     });
+            }
+        },
+        selectCategory(event){
+            if (event === 'movie') {
+                movieType = 'movie'
+            } else {
+                movieType = 'series'
             }
         },
         showMovieInfo(){
@@ -52,12 +61,10 @@ const App = {
                     console.log(err);
                 });
         },
-        showFavorites() {
-            this.showFavoritesList = true;
-        },
         getFavorites(){
-            const localStorage = localStorage.getItem('favorite_films').findIndex(el => el.imdbID);
-            this.showFavorites();
+            this.showFavoritesList = true;
+            this.favorites = JSON.parse(localStorage.getItem('favorite_films'))
+            this.searchResult = false;
         },
         addRemoveFavorites(id){
             const index = this.movieList.findIndex(el => el.imdbID === id);
@@ -82,12 +89,37 @@ const App = {
             });
             return arr
         }
-    }, created() {
-        // записати сюди масив зі стореджа і відобразити в фейворітсах
-    }, components: {
-        movieItem
-    },
+    }
 }
 
 Vue.createApp(App).mount('#app')
 
+/*
+
+Actors: "Robert Pattinson, Zoë Kravitz, Jeffrey Wright"
+Awards: "2 wins & 7 nominations"
+BoxOffice: "$369,345,583"
+Country: "United States"
+DVD: "19 Apr 2022"
+Director: "Matt Reeves"
+Genre: "Action, Crime, Drama"
+Language: "English, Spanish, Latin, Italian"
+Metascore: "72"
+Plot: "When a sadistic serial killer begins murdering key political figures in Gotham, Batman is forced to investigate the city's hidden corruption and question his family's involvement."
+Poster: "https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg"
+Production: "N/A"
+Rated: "PG-13"
+Ratings: [{Source: "Internet Movie Database", Value: "7.9/10"}, {Source: "Rotten Tomatoes", Value: "85%"},…]
+Released: "04 Mar 2022"
+Response: "True"
+Runtime: "176 min"
+Title: "The Batman"
+Type: "movie"
+Website: "N/A"
+Writer: "Matt Reeves, Peter Craig, Bill Finger"
+Year: "2022"
+imdbID: "tt1877830"
+imdbRating: "7.9"
+imdbVotes: "510,059"
+
+*/
